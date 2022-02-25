ccf0edd5cf7764d67d2740aa0e2cd18cc503c937
halfjoe <halfjoe@outlook.com>
2022-02-25 17:13:48 +0800

    Update losses.py

--- texture_style_transfer/transfer/losses.py
+++ texture_style_transfer/transfer/losses.py
@@ -77,21 +77,6 @@ def gram_matrix(y):
     G.div_(h * w)
     return G
 
-# def gram_matrix(input):
-#     a, b, c, d = input.size()  # a=batch size(=1)
-#     # b=number of feature maps
-#     # (c,d)=dimensions of a f. map (N=c*d)
-#
-#     features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
-#
-#     G = torch.mm(features, features.t())  # compute the gram product
-#
-#     # we 'normalize' the values of the gram matrix
-#     # by dividing by the number of element in each feature maps.
-#     # return G.div(a * b * c * d)
-#     return G
-
-
 
 class ContentLossGatys(nn.Module):
     def __init__(self, content_image, face_mask=None):
